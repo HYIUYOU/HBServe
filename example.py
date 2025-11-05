ce6026e973bbf2e0c87555e031513747ed604a14
HYIUYOU <821219285@qq.com>
2025-11-05 15:05:21 +0800

    debug

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from transformers import AutoTokenizer
 
 
 def main():
-    path = os.path.expanduser("/home/admin/workspace/aop_lab/app_data/.cache/models--Qwen--Qwen3-8B/snapshots/b968826d9c46dd6066d109eabc6255188de91218")
+    path = os.path.expanduser("/home/admin/workspace/aop_lab/app_data/.cache/models--facebook--opt-6.7b/snapshots/a45aa65bbeb77c1558bc99bedc6779195462dab0")
     tokenizer = AutoTokenizer.from_pretrained(path)
     llm = LLM(path, enforce_eager=True, tensor_parallel_size=1, gpu_memory_utilization=0.6)
 
@@ -12,30 +12,33 @@ def main():
     prompts = [
         "introduce yourself",
         "describe the benefits of model parallelism",
-        "introduce yourself",
-        "list all prime numbers within 100",
-        "introduce yourself",
-        "list all prime numbers within 100",
-        "introduce yourself",
-        "list all prime numbers within 100",
-        "introduce yourself",
-        "list all prime numbers within 100",
-        "introduce yourself",
-        "list all prime numbers within 100",
-        "introduce yourself",
-        "list all prime numbers within 100",
-        "introduce yourself",
-        "list all prime numbers within 100",
+        # "introduce yourself",
+        # "list all prime numbers within 100",
+        # "introduce yourself",
+        # "list all prime numbers within 100",
+        # "introduce yourself",
+        # "list all prime numbers within 100",
+        # "introduce yourself",
+        # "list all prime numbers within 100",
+        # "introduce yourself",
+        # "list all prime numbers within 100",
+        # "introduce yourself",
+        # "list all prime numbers within 100",
+        # "introduce yourself",
+        # "list all prime numbers within 100",
     ]
-    prompts = [
-        tokenizer.apply_chat_template(
-            [{"role": "user", "content": prompt}],
-            tokenize=False,
-            add_generation_prompt=True,
-            enable_thinking=True
-        )
-        for prompt in prompts
-    ]
+    from HBserve.utils.loader import load_longbench_prompts
+    jsonl_file = "/home/admin/workspace/aop_lab/app_source/data/longbench/2wikimqa_e.jsonl"
+    prompts = load_longbench_prompts(jsonl_file, max_samples=50)  
+    # prompts = [
+    #     tokenizer.apply_chat_template(
+    #         [{"role": "user", "content": prompt}],
+    #         tokenize=False,
+    #         add_generation_prompt=True,
+    #         enable_thinking=True
+    #     )
+    #     for prompt in prompts
+    # ]
     import time
     t1 = time.time()
     outputs = llm.generate(prompts, sampling_params)
